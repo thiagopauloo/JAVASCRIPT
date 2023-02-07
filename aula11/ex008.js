b66c5c2133ede876b6e808d3d1a00103870eb1fb
thiagopauloo <thiaggopaulo@gmail.com>
2023-02-06 21:39:59 -0300

    exercicius js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // Concição simples!
+    console.log(`Vocé ultrapassou a velocidade permitida. MULTADO!`)
+}  
+console.log(`Dirija sempre usando o cinto de segurança!`)
