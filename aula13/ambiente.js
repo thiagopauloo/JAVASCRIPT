d880de35fcae4c50a45e26df1cd0d501ebbcf0a2
thiagopauloo <thiaggopaulo@gmail.com>
2023-02-09 16:21:15 -0300

    aula 013 js

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+do {
+    (c <= 10)
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 7)
+
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
