b5e8d89598ae71fee0bfc64b0247edce3d6d9e94
thiagopauloo <thiaggopaulo@gmail.com>
2023-02-13 21:17:24 -0300

    exercicio js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,11 +1,18 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || fano.value > ano) {
-        window.alert('[ERRO] Verificar os dados e tenete novamente!')
-    } else {
-        window.alert('tudo ok')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    }else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
